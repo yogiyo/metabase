033d526f828aff0d9f21f38122d0a96f61cbd5d4
Kyle Doherty <kyle.l.doherty@gmail.com>
2017-06-27 10:39:33 -0700

    initial ui for bin settings

--- frontend/src/metabase/admin/datamodel/containers/FieldApp.jsx
+++ frontend/src/metabase/admin/datamodel/containers/FieldApp.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router'
+import cx from 'classnames'
 
 import Button from 'metabase/components/Button'
 import Icon from 'metabase/components/Icon'
 import Input from 'metabase/components/Input'
 import Select from 'metabase/components/Select'
+import CheckBox from 'metabase/components/CheckBox'
 
 const SelectClasses = 'h3 border-dark shadowed p2'
 
@@ -34,6 +36,9 @@ export default class FieldApp extends Component {
                         <FieldRemapping />
                     </Section>
 
+                    <Section>
+                        <FieldBinning />
+                    </Section>
                     { /*
                     <Section>
                         <FieldCache />
@@ -232,3 +237,112 @@ class FieldRemapping extends Component {
     }
 }
 
+class FieldBinning extends Component {
+    render () {
+        return (
+            <div>
+                <SectionHeader title="Bins" />
+
+                <ol>
+                    <li className="mb1">
+                        <Bin
+                            name="Default"
+                            enabled={true}
+                        />
+                    </li>
+                    <li className="mb1">
+                        <Bin
+                            name="My custom bin"
+                            enabled={false}
+                            expanded={true}
+                        />
+                    </li>
+                </ol>
+            </div>
+        )
+    }
+}
+
+const BinStrategy = ({ name, selected, children }) =>
+    <div className="relative">
+        <div
+            className={cx(
+                'circle bordered absolute',
+                { 'bg-brand border-brand': selected },
+                { 'bg-white': !selected }
+            )}
+            style={{ borderWidth: 2, width: 20, height: 20 }}
+        >
+        </div>
+        <div className="ml3">
+            <h3>{name}</h3>
+            { selected && children && <div className="mt2">{children}</div> }
+        </div>
+    </div>
+
+
+const BinFormLabel = ({ children }) =>
+    <h4 className="text-italic mb1">{children}</h4>
+
+const Bin = ({ name, enabled, expanded }) =>
+    <div className="bordered border-dark p3 rounded text-measure">
+        <div className={cx({ 'flex align-center': !expanded })}>
+            { !expanded && <CheckBox checked={enabled} /> }
+            { expanded
+                ? [
+                    <BinFormLabel>Name</BinFormLabel>,
+                    <Input
+                        className="AdminInput input block full"
+                        type="text"
+                        value={name}
+                    />
+                ]
+                : <h3 className="ml1">{name}</h3>
+            }
+            { expanded && (
+                <div className="mt2">
+                    <BinFormLabel>Bin Strategy</BinFormLabel>
+                    <ol>
+                        <li className="mb1">
+                            <BinStrategy name="Natural" selected={false} />
+                        </li>
+                        <li className="mb1">
+                            <BinStrategy
+                                name="Pick size"
+                                selected={true}
+                            >
+                                <div className="Grid Grid--1of3 Grid--gutters Grid--fit">
+                                    <div className="Grid-cell">
+                                        <BinFormLabel>Min</BinFormLabel>
+                                        <Input className="input flex" placeholder="0" />
+                                    </div>
+
+                                    <div className="Grid-cell">
+                                        <BinFormLabel>Max</BinFormLabel>
+                                        <Input className="input flex" placeholder="100" />
+                                    </div>
+
+                                    <div className="Grid-cell">
+                                        <BinFormLabel>Bin width</BinFormLabel>
+                                        <Input className="input flex" placeholder="100" />
+                                    </div>
+                                </div>
+                            </BinStrategy>
+                        </li>
+                        <li className="mb1">
+                            <BinStrategy name="Pick number of bins" selected={false} />
+                        </li>
+                        <li className="mb1">
+                            <BinStrategy name="Custom" selected={false} />
+                        </li>
+                    </ol>
+                    <div className="flex align-center mt2">
+                        <div className="ml-auto">
+                            <Button borderless>Cancel</Button>
+                            <Button primary>Save</Button>
+                        </div>
+                    </div>
+                </div>
+            )}
+        </div>
+    </div>
