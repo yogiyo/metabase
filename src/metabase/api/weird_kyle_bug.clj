fd1e145f1ed0da63c316295ce20d2ca0d1b3e685
Simon Belak <simon.belak@gmail.com>
2017-08-09 18:43:54 +0200

    weird kyle bug -> add explicit casts

--- src/metabase/api/weird_kyle_bug.clj
+++ src/metabase/api/weird_kyle_bug.clj
@@ -6,7 +6,7 @@
 
 (api/defendpoint GET "/test1/:id1/:id2"
   [id1 id2]
-  (map (partial api/read-check Segment) [id1 id2])
+  (dorun (map (partial api/read-check Segment) [id1 id2]))
   "ok")
 
 (api/defendpoint GET "/test2/:id1"
@@ -22,6 +22,27 @@
 (api/defendpoint GET "/test4/:id1/:id2"
   [id1 id2]
   [(Segment id1) (Segment id2)]
+  "ok")
+
+(api/defendpoint GET "/test5/:id1/:id2"
+  [id1 id2]
+  (dorun (map (partial api/read-check Segment) [(Integer/parseInt id1)
+                                                (Integer/parseInt id2)]))
+  "ok")
+
+(api/defendpoint GET "/test6/:id1"
+  [id1]
+  (api/read-check Segment (Integer/parseInt id1))
+  "ok")
+
+(api/defendpoint GET "/test7/:id1"
+  [id1]
+  (Segment (Integer/parseInt id1))
+  "ok")
+
+(api/defendpoint GET "/test8/:id1/:id2"
+  [id1 id2]
+  [(Segment (Integer/parseInt id1))  (Segment (Integer/parseInt id2))]
   "ok")
 
 (api/define-routes)
