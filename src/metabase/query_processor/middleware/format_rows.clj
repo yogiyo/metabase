75c0062c83767fe8a45a5929a22e756479671915
Ryan Senior <ryan@metabase.com>
2017-08-23 11:18:42 -0500

    Hacked up differentiation between date and datetime on the backend

--- src/metabase/query_processor/middleware/format_rows.clj
+++ src/metabase/query_processor/middleware/format_rows.clj
@@ -3,16 +3,20 @@
    Currently, the only thing this does is convert datetime types to ISO-8601 strings in the appropriate timezone."
   (:require [metabase.util :as u]))
 
-(defn- format-rows* [{:keys [report-timezone]} rows]
+(defn- format-rows* [{:keys [report-timezone]} {:keys [cols rows] :as results}]
   (let [timezone (or report-timezone (System/getProperty "user.timezone"))]
-    (for [row rows]
-      (for [v row]
-        (if (u/is-temporal? v)
-          ;; NOTE: if we don't have an explicit report-timezone then use the JVM timezone
-          ;;       this ensures alignment between the way dates are processed by JDBC and our returned data
-          ;;       GH issues: #2282, #2035
-          (u/->iso-8601-datetime v timezone)
-          v)))))
+    (assoc results :rows
+           (for [row rows]
+             (map (fn [col v]
+                    (if (u/is-temporal? v)
+                      ;; NOTE: if we don't have an explicit report-timezone then use the JVM timezone
+                      ;;       this ensures alignment between the way dates are processed by JDBC and our returned data
+                      ;;       GH issues: #2282, #2035
+                      (if (= :type/Date (:base_type col))
+                        (clj-time.format/unparse (clj-time.format/formatters :date) (clj-time.coerce/from-date v))
+                        (u/->iso-8601-datetime v timezone))
+                      v))
+                  cols row)))))
 
 (defn format-rows
   "Format individual query result values as needed.  Ex: format temporal values as iso8601 strings w/ timezone."
@@ -21,4 +25,4 @@
     (let [results (qp query)]
       (if-not (:rows results)
         results
-        (update results :rows (partial format-rows* settings))))))
+        (format-rows* settings results)))))
